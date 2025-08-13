fe8fbd7eaa9a5a54c1bea306c1c0c7335678689b
Nataliya Korobitsina <toropova@chemomsu.ru>
2025-08-13 17:06:19 +0600

    Задания для урока № 3

---
+++ test1.py
@@ -0,0 +1,17 @@
+# задание 1
+a=input('Укажите вид питомца: ')
+b=input('Укажите кличку питомца: ')
+c=input('Укажите возраст питомца, указав месяцев или лет: ')
+print('Это', a, 'по кличке', b, end='. ')
+print('Возраст:', c)
+
+# задание 2
+print('Напишите 7 этапов эволюции человека, начиная с самого древнего.')
+a=input() # dryopithecus
+b=input() # australopitecus
+c=input() # homo habilis
+d=input() # homo erectus
+e=input() # homo heidelbergensis
+f=input() # homo sapiens
+g=input() # homo sapiens sapiens
+print(a, b, c, d, e, f, g, sep=' => ')
